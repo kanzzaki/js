59072186c505c39136be0f00d82c6f9b5a3cf17d
Alan <sozaev16@gmail.com>
2020-12-11 23:28:59 +0300

    Это первый коммит

---
+++ lesson 1/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var obj = {
+    name: "Alan",
+    age: 20,
+    isMarried: false,
+};
+
+console.log(obj["isMarried"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[1]);
+
+//alert("Hello world");
+
+/* let answer = confirm("Hello world?");
+console.log(answer); */
+
+//let answer = prompt("Hello world?", "if");
+console.log(typeof(arr));
+
+// `1` '2' "3"
